d07679e6fb9a06e60eb6a3fcea8f3c2cb463d6fa
James Kelly <jk377y@gmail.com>
2023-05-30 17:59:31 -0500

    basic menu structure laid out

--- morsecode.py
+++ morsecode.py
@@ -22,3 +22,26 @@ def morse_to_text(morse_code, morse_dict):
     return (text)
 
 
+# need menu and it should loop until user chooses to exit
+
+while True:
+    print("\n==== Morse Code Translator ====")
+    print("1. Text to Morse code")
+    print("2. Morse code to Text")
+    print("3. Exit")
+
+    # get the user's choice
+    choice = input("\nEnter your choice (1-3): ")
+
+
+    if choice == "1":
+        text_to_morse()
+
+    elif choice == "2":
+        morse_to_text()
+    
+    elif choice == "3":
+        break
+
+    else:
+        print("Invalid choice!")
